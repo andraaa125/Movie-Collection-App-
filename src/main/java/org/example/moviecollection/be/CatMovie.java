55b26bbcb69efaaff8c45cbf3aba2a8713fde9ac
Abdurahman Haji Abdi <abdulrahmanxambali@gmail.com>
2025-01-07 15:18:37 +0100

    Update CatMovie.java

--- src/main/java/org/example/moviecollection/be/CatMovie.java
+++ src/main/java/org/example/moviecollection/be/CatMovie.java
@@ -1,16 +1,12 @@
 package org.example.moviecollection.be;
 
-import org.example.moviecollection.be.Movie;
-import org.example.moviecollection.be.Category;
-
 public class CatMovie {
     private final int id;
     private int categoryID;
     private int movieID;
     private Movie movie;
     private Category category;
-
-
+    
     public CatMovie(int id, int categoryID, int movieID) {
         this.id = id;
         this.categoryID = category.getId();
