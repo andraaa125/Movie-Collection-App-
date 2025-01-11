092c52028ac992175a97bb08aeff9133fde5b4cb
JaneyFan <fanjaney9@gmail.com>
2025-01-11 14:11:56 +0100

    style css the main window

--- src/main/java/org/example/moviecollection/MovieCollectionApplication.java
+++ src/main/java/org/example/moviecollection/MovieCollectionApplication.java
@@ -11,8 +11,8 @@ public class MovieCollectionApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(MovieCollectionApplication.class.getResource("Movie-Collection-Application.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        Scene scene = new Scene(fxmlLoader.load());
+        stage.setTitle("Movie");
         stage.setScene(scene);
         stage.show();
     }
